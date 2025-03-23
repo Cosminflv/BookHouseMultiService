0bb20a383cb9b03be121ef9e7f362f9682896aff
cosmin <cosmin_popovici2012@yahoo.com>
2025-03-23 17:58:27 +0200

    Added Thymeleaf project

---
+++ BookHouseGUI/src/main/java/org/example/bookhousegui/BookHouseGuiApplication.java
@@ -0,0 +1,13 @@
+package org.example.bookhousegui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookHouseGuiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookHouseGuiApplication.class, args);
+    }
+
+}
---
+++ BookHouseGUI/src/test/java/org/example/bookhousegui/BookHouseGuiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bookhousegui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookHouseGuiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
